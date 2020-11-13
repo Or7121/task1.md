58cfd3cfa9028ffa2e6b6331ba4340364b074226
Or7121 <72847126+Or7121@users.noreply.github.com>
2020-11-13 10:20:07 +0900

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,96 @@
+A = input("文字1入力：")
+B = input("文字2入力：")
+C = input("文字3入力：")
+
+if A == '0':
+    A="Number"
+
+elif A== '1': 
+    A= "Number"
+elif A== '2': 
+    A= "Number"
+elif A== '3': 
+    A= "Number"
+elif A== '4': 
+    A= "Number"
+elif A== '5': 
+    A= "Number"
+elif A== '6': 
+    A= "Number"
+elif A== '7': 
+    A= "Number"
+elif A== '8': 
+    A= "Number"
+elif A== '9': 
+    A= "Number"
+elif A== '+': 
+    A= "Plus"
+elif A== '-': 
+    A= "Minus"
+elif A== '×': 
+    A= "kakeru"
+elif A== '÷': 
+    A= "waru"
+
+
+if B  == '0':
+    B ="Number"
+
+elif B == '1': 
+    B = "Number"
+elif B == '2': 
+    B = "Number"
+elif B== '3': 
+    B = "Number"
+elif B == '4': 
+    B = "Number"
+elif  B == '5': 
+    B = "Number"
+elif B == '6': 
+    B = "Number"
+elif B == '7': 
+    B = "Number"
+elif B == '8': 
+    B = "Number"
+elif B == '9': 
+    B = "Number"
+elif B== '+': 
+    B= "Plus"
+elif B== '-': 
+    B= "Minus"
+elif B== '×': 
+    B= "kakeru"
+elif B== '÷': 
+    B= "waru"
+
+if C == '0':
+    C="Number"
+
+elif C== '1': 
+    C= "Number"
+elif C== '2': 
+    C= "Number"
+elif C== '3': 
+    C= "Number"
+elif C== '4': 
+    C= "Number"
+elif C== '5': 
+    C= "Number"
+elif C== '6': 
+    C= "Number"
+elif C== '7': 
+    C= "Number"
+elif C== '8': 
+    C= "Number"
+elif C== '9': 
+    C= "Number"
+elif C== '+': 
+    C= "Plus"
+elif C== '-': 
+    C= "Minus"
+elif C== '×': 
+    C= "kakeru"
+elif C== '÷': 
+    C= "waru"
+
+print (A ,B , C ,'EOF')
